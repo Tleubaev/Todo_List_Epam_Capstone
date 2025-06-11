08d4eef90e3a9dde1c2199cad3e2d9539d1f601b
Oralbek Tleubayev <o.tleubayev@elemental.kz>
2025-06-11 11:38:03 +0500

    Migration update

---
+++ TodoListApp.Services.Database/Migrations/20250611063333_AddTagAndComment.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace TodoListApp.Services.Database.Migrations
+{
+    public partial class AddTagAndComment : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Role",
+                table: "Users",
+                type: "character varying(20)",
+                maxLength: 20,
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<Guid>(
+                name: "AssignedUserId",
+                table: "TaskItems",
+                type: "uuid",
+                nullable: false,
+                defaultValue: new Guid("00000000-0000-0000-0000-000000000000"));
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "CreatedAt",
+                table: "TaskItems",
+                type: "timestamp with time zone",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.CreateIndex(
+                name: "IX_TaskItems_AssignedUserId",
+                table: "TaskItems",
+                column: "AssignedUserId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_TaskItems_Users_AssignedUserId",
+                table: "TaskItems",
+                column: "AssignedUserId",
+                principalTable: "Users",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_TaskItems_Users_AssignedUserId",
+                table: "TaskItems");
+
+            migrationBuilder.DropIndex(
+                name: "IX_TaskItems_AssignedUserId",
+                table: "TaskItems");
+
+            migrationBuilder.DropColumn(
+                name: "Role",
+                table: "Users");
+
+            migrationBuilder.DropColumn(
+                name: "AssignedUserId",
+                table: "TaskItems");
+
+            migrationBuilder.DropColumn(
+                name: "CreatedAt",
+                table: "TaskItems");
+        }
+    }
+}
